51761b0370e207688f63ed46824b878b191e2bfe
Vladislav Gudov <46536864+Vladislavius12@users.noreply.github.com>
2021-11-29 16:59:01 +0300

    Try PostgreSQL

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+)
+
+const(
+	host = "localhost"
+	port = 5300
+	user = "postgres"
+	password =" "
+	dbname = "first_db"
+)
+
+func main() {
+	// Connect to DB
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user , password, dbname)
+
+	db, err := sql.Open("postgres", psqlconn)
+	CheckError(err)
+
+	defer db.Close()
+
+	insertStmt := `insert into "Employee("Name", "EmpID") values('Rohit', 21)`
+	_, e := db.Exec(insertStmt)
+	CheckError(e)
+
+	insertDynStmt := `insert into "Employee" ("Name", "EmpId") values($1, $2)`
+	_, e := db.Exec(insertDynStmt, "krish", 03)
+	CheckError(e)
+
+}
+	func CheckError(err error){
+		if err != nil{
+			panic(err)
+		}
+	}
